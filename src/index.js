5c0fac164747874f147d53c93418d59d257ea0a5
Alpha5959 <109584578+Alpha5959@users.noreply.github.com>
2023-12-24 09:56:17 +0600

    Major Update | index.js
    This update enhances the Gemini text generation module by improving error handling for clearer diagnostics. Users can customize text generation parameters with greater flexibility, and the code is optimized for efficiency and readability. Also, fixed few bugs.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 const axios = require('axios');
 
-let config = {}
+let config = {};
 
 function setConfig(newConfig) {
   config = newConfig;
 }
 
-async function createText(text) {
+function validateConfig() {
   if (!config.apikey || !config.key) {
     throw new Error("API key and key must be set before making a request.");
   }
+}
+
+async function createText(text) {
+  validateConfig();
+
+  const {
+    apikey,
+    key,
+    temperature = 0.7,
+    top_k: topK = 1,
+    top_p: topP = 1,
+    maxOutputTokens = 2000,
+    endpoint = 'https://google-bard1.p.rapidapi.com/v1/gemini/gemini-pro',
+  } = config;
 
   const requestOptions = {
     method: 'GET',
-    url: 'https://google-bard1.p.rapidapi.com/v1/gemini/gemini-pro',
+    baseURL: endpoint,
     headers: {
-      'api_key': config.apikey,
-      text: encodeURIComponent(text),
-      temperature:config.temperature || 0.7,
-      top-k: config.top_k || 1,
-      top-p: config.top_p || 1,
-      maxOutputTokens: config.maxOutputTokens || 2000
-      'X-RapidAPI-Key': config.key,
+      'api_key': apikey,
+      'text': encodeURIComponent(text),
+      'temperature': config.temperature || temperature,
+      'top-k': config.top_k || topK,
+      'top-p': config.top_p || topP,
+      'maxOutputTokens': config.maxOutputTokens || maxOutputTokens,
+      'X-RapidAPI-Key': key,
       'X-RapidAPI-Host': 'google-bard1.p.rapidapi.com',
     },
   };
@@ -39,4 +53,4 @@ const Gemini = {
   createText,
 };
 
-module.exports =  Gemini;
+module.exports = Gemini;
